49a38d0ef0c3b71b2d0bf4840b71bcae829ddf63
Jean Carlos Zhong <64503277+jeancarlosZ@users.noreply.github.com>
2022-02-13 21:29:53 -0500

    feat: app is connected to the FourSquare API and can render data from the API for homepage and individual page

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -5,27 +5,28 @@ import Image from "next/image";
 
 import cls from "classnames";
 
-import coffeeStoresData from "../../data/coffee-stores.json";
-
 import styles from "../../styles/coffee-store.module.css";
+import { fetchCoffeeStores } from "../../lib/coffee-stores";
 
-export function getStaticProps(staticProps) {
+export async function getStaticProps(staticProps) {
   const params = staticProps.params;
+  const coffeeStores = await fetchCoffeeStores();
 
   return {
     props: {
-      coffeeStore: coffeeStoresData.find(coffeeStore => {
-        return coffeeStore.id.toString() === params.id;
+      coffeeStore: coffeeStores.find(coffeeStore => {
+        return coffeeStore.fsq_id.toString() === params.id;
       }),
     },
   };
 }
 
-export function getStaticPaths() {
-  const paths = coffeeStoresData.map(coffeeStore => {
+export async function getStaticPaths() {
+  const coffeeStores = await fetchCoffeeStores();
+  const paths = coffeeStores.map(coffeeStore => {
     return {
       params: {
-        id: coffeeStore.id.toString(),
+        id: coffeeStore.fsq_id.toString(),
       },
     };
   });
@@ -42,7 +43,8 @@ const CoffeeStore = props => {
     return <div>Loading...</div>;
   }
 
-  const { address, name, neighbourhood, imgUrl } = props.coffeeStore;
+  const { name, imgUrl } = props.coffeeStore;
+  const { address, neighborhood } = props.coffeeStore.location;
 
   const handleUpvoteButton = () => {
     console.log("handle upvote");
@@ -64,7 +66,10 @@ const CoffeeStore = props => {
             <h1 className={styles.name}>{name}</h1>
           </div>
           <Image
-            src={imgUrl}
+            src={
+              imgUrl ||
+              "https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"
+            }
             width={600}
             height={360}
             className={styles.storeImg}
@@ -77,10 +82,12 @@ const CoffeeStore = props => {
             <Image src="/static/icons/places.svg" width="24" height="24" alt="" />
             <p className={styles.text}>{address}</p>
           </div>
-          <div className={styles.iconWrapper}>
-            <Image src="/static/icons/nearMe.svg" width="24" height="24" alt="" />
-            <p className={styles.text}>{neighbourhood}</p>
-          </div>
+          {neighborhood && (
+            <div className={styles.iconWrapper}>
+              <Image src="/static/icons/nearMe.svg" width="24" height="24" alt="" />
+              <p className={styles.text}>{neighborhood}</p>
+            </div>
+          )}
           <div className={styles.iconWrapper}>
             <Image src="/static/icons/star.svg" width="24" height="24" alt="" />
             <p className={styles.text}>1</p>
