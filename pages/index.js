7878783298f596f5af942e8625bb28c18bff6e33
Jean Carlos Zhong <64503277+jeancarlosZ@users.noreply.github.com>
2022-02-19 13:14:00 -0500

    feat: can make API call on the server side

--- pages/index.js
+++ pages/index.js
@@ -30,13 +30,17 @@ export default function Home(props) {
     const setCoffeeStoresByLocation = async () => {
       if (latLong) {
         try {
-          const fetchedCoffeeStores = await fetchCoffeeStores(latLong, 30);
+          const response = await fetch(
+            `/api/getCoffeeStoresByLocation?latLong=${latLong}&limit=30`,
+          );
+          const coffeeStores = await response.json();
           dispatch({
             type: ACTION_TYPES.SET_COFFEE_STORES,
             payload: {
-              coffeeStores: fetchedCoffeeStores,
+              coffeeStores,
             },
           });
+          setCoffeeStoresError("");
         } catch (error) {
           console.log({ error });
           setCoffeeStoresError(error.message);
